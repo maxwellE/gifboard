12f03709f3e30baa5c799ad9441f529aaf93925d
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-07-20 21:55:53 -0700

    redesign

--- client/gifboard.js
+++ client/gifboard.js
@@ -1,42 +1,50 @@
 Meteor.startup(function() {
-    $(document).ready(function(){
-        $('#tags').tagit();
-    });
 });
 
 Meteor.subscribe("gifs");
 Template.add_gif.events({
     'click #submit_gif': function(e, template){
-        var src,tags,regex;
+        var src,tags;
         src = template.find("#src").value;
         tags = template.find("#tags").value;
-        regex = /.+\.gif$/;
-        if(regex.test(src)){
-              Gifs.insert({
-                user_id: Meteor.userId(),
-                src: src,
-                tags: prepTags(tags)
-              }
-           );
-           template.find("#src").value = "";
-           template.find("#tags").value = "";
-        }else{
-            $('#src').addClass("bad_src");
-        }
+        Gifs.insert({
+          user_id: Meteor.userId(),
+          src: src,
+          tags: prepTags(tags)
+        });
+        template.find("#src").value = '';
+        $("#tags").tagit("removeAll");
+     },
+     'keyup, change #src': function(e, template){
+         var src, regex;
+         src = template.find("#src").value;
+         regex = /.+\.gif$/;
+         if(regex.test(src)){
+            template.find('#submit_gif').disabled = false;
+         }else{
+            template.find('#submit_gif').disabled = true;
+         }
      }
-  }
+   }
 );
+
+Template.gifcount.count = function(){
+    return Gifs.find().count(); 
+}
+
 Template.gifs_list.gifs = function(){
-    return Gifs.find({}, {sort: {_id: 1}} ).fetch();
+    return Gifs.find();
 };
 
 Template.gifs_list.rendered = function(){
-    $(document).ready(function(){
-        var container = document.querySelector('#container');
-        $('#container').imagesLoaded( function() {
-          pckry = new Packery( container );
-        });
-    });
+   var container = document.querySelector('#container');
+   $('#container').imagesLoaded( function() {
+     pckry = new Packery( container );
+   });
+}
+
+Template.add_gif.rendered = function(){
+  $('#tags').tagit();
 }
 
 function prepTags(tags){
